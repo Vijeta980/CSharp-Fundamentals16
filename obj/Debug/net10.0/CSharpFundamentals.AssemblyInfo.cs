3e188ee260100ebd60b148b53c2b9d7bfd45dd2a
Vijeta Yadav <yadavvijeta878@gmail.com>
2025-12-28 00:16:32 +0530

    Restore full C# Fundamentals project

--- obj/Debug/net10.0/CSharpFundamentals.AssemblyInfo.cs
+++ obj/Debug/net10.0/CSharpFundamentals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpFundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7b391b55d89a3175b419757e0c8adc4d94e8f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpFundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpFundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

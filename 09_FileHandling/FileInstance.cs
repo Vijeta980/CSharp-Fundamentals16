3821b5ffb148b7f3225bc17403a7320ad899b62c
Vijeta Yadav <yadavvijeta878@gmail.com>
2025-12-28 23:37:30 +0530

    Added FileHandling examples

---
+++ 09_FileHandling/FileInstance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals._09_FileHandling
+{
+    internal class FileInstance
+    {
+        public static void Run()
+        {
+            var fileInfo = new FileInfo("text.txt");
+
+            //check if file exists
+            if (!fileInfo.Exists)
+            {
+                using (var stream = fileInfo.Create())
+                {
+                    //file created
+                }
+                Console.WriteLine("File created");
+
+            }
+            if (fileInfo.Exists)
+            {
+                Console.WriteLine("File exists");
+            }
+
+            //Copy file
+            fileInfo.CopyTo("copy.txt", true);
+
+            //delete file
+            //fileInfo.Delete();
+
+            //properties
+            Console.WriteLine(fileInfo.Name);
+            Console.WriteLine(fileInfo.FullName);
+            Console.WriteLine(fileInfo.Length);
+            Console.WriteLine(fileInfo.Extension);
+
+            //Creation time
+            Console.WriteLine(fileInfo.CreationTime);
+        }
+    }
+}
---
+++ 09_FileHandling/FileStatic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals._09_FileHandling
+{
+    internal class FileStatic
+    {
+        public static void Run()
+        {
+            //var path = @"C:\Temp\test.txt";
+
+            //if (File.Exists(path))
+            //{
+            //    var content = File.ReadAllText(path);
+            //    Console.WriteLine(content); 
+            //}
+            //else
+            //{
+            //    File.WriteAllText(path, "File created by C#");
+            //    Console.WriteLine("File Created");
+            //}
+
+            var directory = @"C:\Temp";
+            var path = Path.Combine(directory, "text.txt");
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            if (File.Exists(path))
+            {
+                var content=File.ReadAllText(path);
+                Console.WriteLine(content);
+            }
+            else
+            {
+                File.WriteAllText(path, "File Created by C#");
+                Console.WriteLine("File created");
+            }
+        }
+    }
+}

48c653b64e01c1dff7bef9cef1001c05398bac33
Vijeta Yadav <yadavvijeta878@gmail.com>
2026-01-07 23:38:08 +0530

    Remove Duplicates

--- PracticeQue1/RemoveDuplicates.cs
+++ PracticeQue1/RemoveDuplicates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,22 +9,47 @@ namespace CSharpFundamentals.PracticeQue1
 {
     public class RemoveDuplicates
     {
-        public static void Run()
+        public static int Run()
         {
-            int[] input = { 1, 2, 2, 3, 4, 4, 5 }; 
+            int[] input = { 1, 2, 2, 3, 4, 4, 5 };
 
-            HashSet<int> uniqueSet = new HashSet<int>();
+            //Extra space approach
 
-            for (int i = 0; i < input.Length; i++)
+            //HashSet<int> uniqueSet = new HashSet<int>();
+
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    uniqueSet.Add(input[i]);
+            //}
+            //foreach(var n in uniqueSet)
+            //{
+            //    Console.Write(n + " ");
+            //}
+
+            //In-place approach
+
+            if(input.Length == 0)
             {
-                uniqueSet.Add(input[i]);
+                return 0;
             }
-            foreach(var n in uniqueSet)
+            int i = 0;
+            for(int j=1;j<input.Length;j++)
             {
-                Console.Write(n + " ");
+                if (input[j] != input[i])
+                {
+                    i++;
+                    input[i] = input[j];
+                }
+            }
+            int k = i + 1;
+
+            for (int x = 0; x < k; x++)
+            {
+                Console.Write(input[x] + " ");
             }
 
-
+            return k;
         }
+        
     }
 }

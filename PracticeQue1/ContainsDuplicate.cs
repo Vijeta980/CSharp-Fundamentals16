a7289702283c4514932bb2fc02a0bd25beef8693
Vijeta Yadav <yadavvijeta878@gmail.com>
2026-01-31 23:27:27 +0530

    Contains Duplicate

---
+++ PracticeQue1/ContainsDuplicate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.PracticeQue1
+{
+    public class ContainsDuplicate
+    {
+        public static void Run()
+        {
+            int[] nums = { 1, 2, 3, 1 };
+            bool result = ContainsDuplicateMethod(nums);
+            Console.WriteLine(result);
+        }
+
+        public static bool ContainsDuplicateMethod(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (set.Contains(num))
+                {
+                    return true; 
+                }
+                set.Add(num);
+            }
+
+            return false; 
+        }
+    }
+}
